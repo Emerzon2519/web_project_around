5d32f7e9c6038db13107df6ae9fb40f68d8a0b51
Emerzon <the_emerzon1999@hotmail.com>
2023-12-04 19:11:16 -0500

    cambios3

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-// let page = document.querySelector(".page");
-// let popup = document.querySelector(".popup");
-// let edit = document.querySelector(".header__icono");
-// let hidden = document.querySelector(".close");
-// let title = document.querySelector(".header__title");
-// let subtitle = document.querySelector(".header__subtitle");
-// let saveInfo = document.querySelector(".popup__button-container");
+let page = document.querySelector(".page");
+let popup = document.querySelector(".popup");
+let edit = document.querySelector(".header__icono");
+let hidden = document.querySelector(".close");
+let title = document.querySelector(".header__title");
+let subtitle = document.querySelector(".header__subtitle");
+let saveInfo = document.querySelector(".popup__button-container");
 
-// let inputName = document.querySelector(".popup__input-name");
-// let inputWork = document.querySelector(".popup__input-work");
+let inputName = document.querySelector(".popup__input-name");
+let inputWork = document.querySelector(".popup__input-work");
 
-// function open() {
+function open() {
 
-//     popup.style.display = "block";
-//     hidden.style.display = "block";
-//     // page.style.opacity = "0.9";
+    popup.style.display = "block";
+    hidden.style.display = "block";
+    // page.style.opacity = "0.9";
     
-//     titleCapture = title.textContent;
-//     inputName.value = titleCapture;
+    titleCapture = title.textContent;
+    inputName.value = titleCapture;
 
-//     subtitleCapture = subtitle.textContent;
-//     inputWork.value = subtitleCapture;
-// }
+    subtitleCapture = subtitle.textContent;
+    inputWork.value = subtitleCapture;
+}
  
-// function closee() {
-//     popup.style.display = "none";
-//     hidden.style.display = "none";
-// }
+function closee() {
+    popup.style.display = "none";
+    hidden.style.display = "none";
+}
 
-// function save(){
+function save(){
 
-//     popup.style.display = "none";
-//     hidden.style.display = "none";
+    popup.style.display = "none";
+    hidden.style.display = "none";
 
-//     nameCapture = inputName.value
-//     title.textContent = nameCapture
+    nameCapture = inputName.value
+    title.textContent = nameCapture
 
-//     workCapture = inputWork.value
-//     subtitle.textContent = workCapture
+    workCapture = inputWork.value
+    subtitle.textContent = workCapture
 
-// }
+}
 
-// edit.addEventListener("click",open); 
+edit.addEventListener("click",open); 
 
-// hidden.addEventListener("click",closee);
-// saveInfo.addEventListener("click",save);
+hidden.addEventListener("click",closee);
+saveInfo.addEventListener("click",save);
