0b5f186d4ff300c4b66f759c5f2e95a4213658e2
Emerzon <the_emerzon1999@hotmail.com>
2023-12-04 21:54:30 -0500

    cambios5

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let popup = document.querySelector(".popup");
-let edit = document.querySelector(".header__icono");
-let hidden = document.querySelector(".close");
-let title = document.querySelector(".header__title");
-let subtitle = document.querySelector(".header__subtitle");
-let saveInfo = document.querySelector(".popup__button-container");
+const popup = document.querySelector(".popup");
+const edit = document.querySelector(".header__icono");
+const hidden = document.querySelector(".close");
+const title = document.querySelector(".header__title");
+const subtitle = document.querySelector(".header__subtitle");
+const saveInfo = document.querySelector(".popup__button-container");
 
-let inputName = document.querySelector(".popup__input-name");
-let inputWork = document.querySelector(".popup__input-work");
+const inputName = document.querySelector(".popup__input-name");
+const inputWork = document.querySelector(".popup__input-work");
 
 function open() {
 
     popup.classList.toggle("popup_show");
     
-    titleCapture = title.textContent;
+    const titleCapture = title.textContent;
     inputName.value = titleCapture;
 
-    subtitleCapture = subtitle.textContent;
+    const subtitleCapture = subtitle.textContent;
     inputWork.value = subtitleCapture;
 }
  
@@ -27,10 +27,10 @@ function save(){
 
     popup.classList.remove("popup_show");
 
-    nameCapture = inputName.value
+    const nameCapture = inputName.value
     title.textContent = nameCapture
 
-    workCapture = inputWork.value
+    const workCapture = inputWork.value
     subtitle.textContent = workCapture
 
 }
